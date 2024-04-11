e7e0cc4092f24808cefb2a66b3e4dad143a0df19
MNVallone <maniva04@gmail.com>
2024-04-11 15:34:03 -0300

    funciones modificadas respecto a las ultimas versiones de los modulos utilizados

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 #PdfToAudio
+
+script simple que permite convertir el texto de un pdf en audio y reproducirlo
+
+Package            Version
+-----------------  -------
+PyPDF2             3.0.1
+pyttsx3            2.90
--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import pyttsx3
 import PyPDF2
 from tkinter.filedialog import *
 
-book = askforfilename()
-pdfreader = PyPDF2.PdfFileReader(book)
-pages = pdfreader.numPages
+book = askopenfilename()
+pdfreader = PyPDF2.PdfReader(book)
+pages = len(pdfreader.pages)
 
 for i in range(0 ,pages):
-    page = pdfreader.getPage(i)
-    text = page.extractText()
+    page = pdfreader.pages[i]
+    text = page.extract_text()
     player = pyttsx3.init()
     player.say(text)
     player.runAndWait()
